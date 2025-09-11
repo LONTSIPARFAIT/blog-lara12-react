a513dd5b1f663e1ed742c9a4b973efe7942f09a6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-11 11:28:29 +0100

    first commit

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -6,7 +6,7 @@ export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
 
     return (
-        <>c
+        <>
             <Head title="Welcome">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
