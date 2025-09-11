bf4dc0343b1514a2ce8b256e765f34e70bd3f08a
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-11 12:52:19 +0100

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Response;
 
 class PostController extends Controller
 {
-    //
+    public function create() : Response
+    {
+        if()
+    }
 }
