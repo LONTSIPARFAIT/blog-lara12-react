372aeb97fc8119be71aada1edd10e1f29d4d02e1
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-11 16:57:03 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
     Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
     Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+    Route::post('/posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
 });
+
+Route::post('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
