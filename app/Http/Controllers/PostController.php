b9a9dd3b571a4b68205bed7bf7137b0cbff2b4f7
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-11 13:21:48 +0100

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Inertia\Response;
 
 class PostController extends Controller
 {
     public function create() : Response
     {
-        if()
+        if(!Auth::check()){
+            abort(403);
+        }
+        return Inertia::render('Post/Create');
     }
 }
