82b62e9a2df1d9365675bae8b395676cbd987cf4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-11 17:09:15 +0100

    first commit

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Post;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        return inertia('Welcome');
+        $posts = Post::with('author')->latest()->get();
+
+        return Inertia::render('welcome', [
+            'posts' => $posts,
+            'canRegister' => config('services.registration.enabled', true)
+        ]);
     }
 }
