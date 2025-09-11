a8277e55952d815636aa0dbfb725d500c647b5a2
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-11 12:45:23 +0100

    first commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'user_id',
+    ];
+
+    protected $appends = ['is_liked', 'likes_count'];
+
+    protected $with = ['likedBy'];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function likedBy()
+    {
+        return $this->belongsToMany(Post::class,  'post_likes');
+    }
+
+    public function getIsLikedAttribute() : bool
+    {
+        return Auth::check() && $this->likedBy->contains('id', Auth::id())
+    }
 }
