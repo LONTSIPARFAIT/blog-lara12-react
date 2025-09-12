dbdbab7ec7915f93804bd11181f45c8b9965abd1
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 09:38:09 +0100

    first commit

--- resources/js/components/nav.tsx
+++ resources/js/components/nav.tsx
@@ -8,7 +8,7 @@ export default function Nav() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
               <div className="flex items-center">
-                <Link href="/" className="text-2xl font-black text-indigo-900">
+                <Link href="/" className="text-2xl font-black text-indigo-600">
                   MyApp
                  </Link> 
               </div>
