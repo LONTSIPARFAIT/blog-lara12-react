b06335e709bc312d6c1022c59f765c8ac48d7dff
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 10:45:15 +0100

    first commit

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,12 +1,17 @@
 import PageProps from '@/types';
+import { Head, Link } from '@inertiajs/react'; 
 import Nav from '@/components/nav';
 import { Post } from '@/types/post';
 
 export default function welcome(auth, posts, canRegister) : PageProps< {posts: Post[], canRegister: boolean }> {
   return (
-    <div>
-        <Nav />
-    </div>
+    <>
+        <Head title='Welcome' />
+        <div className="min-h-screen">
+          <Nav />
+        </div>
+        
+    </>
   )
 }
 
