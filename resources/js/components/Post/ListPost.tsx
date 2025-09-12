d1f0e4c39b640c6ca01b6a28c90d138775b44f5e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 15:32:30 +0100

    first commit

--- resources/js/components/Post/ListPost.tsx
+++ resources/js/components/Post/ListPost.tsx
@@ -1,9 +1,13 @@
 import { usePage } from '@inertiajs/react';
 import { useState } from 'react';
 
-export default function ListPost({posts, showAuthor == true} : Props) {
+export default function ListPost({posts, showAuthor = true} : Props) {
+
     const { auth } = usePage().props as any;
-    const [deletingId, setDeletingId] = useState(false);
+    const [deletingId, setDeletingId] = useState<number | null>(null);
+
+    const handleDelete()
+
   return (
     <div>
       
