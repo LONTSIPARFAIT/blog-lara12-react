75dbab570b40d3a618ee13ec06a728d5c9156f56
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 09:48:30 +0100

    first commit

--- resources/js/components/nav.tsx
+++ resources/js/components/nav.tsx
@@ -23,7 +23,7 @@ export default function Nav() {
                       Connexion
                     </Link>
                     <Link href={route('/dasboard')} className="inline-flex items-center px-4 py-2 border text-sm font-medium rounded-md text-white bg-indigo-600  hover:bg-indigo-700">
-                      Dashboard
+                      Inscription
                     </Link>
                   </>
                 )
