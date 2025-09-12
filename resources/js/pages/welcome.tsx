9874bbd8b846ffa0e6dea4a806c884cc6a40e442
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 11:09:46 +0100

    first commit

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -13,12 +13,13 @@ export default function welcome(auth, posts, canRegister) : PageProps< {posts: P
           <div className="bg-white">
             <div className="max-w-7px mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
               <div className="text-center">
-                <h1 className='text-4xl font-blacktext-gray-800 ' >
-                  <span className="block">
-                    Bienvenue sur
-                  </span>
+                <h1 className='text-4xl font-black text-gray-800 ' >
+                  <span className="block">Bienvenue sur</span>
                   <span className="block text-indigo-700">Notre Blog Communautaire</span>
                 </h1>
+                <p className="mt-3 max-w-md mx-auto text-gray-500">
+                 Explorez des articles variés sur la culture, la technologie et le bien-être. Rejoignez notre communauté et partagez vos idées !
+                </p>
               </div>
             </div>
           </div>
