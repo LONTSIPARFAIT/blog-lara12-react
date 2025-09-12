73a648781dfedaf89f679415ce9204533e9a3dc9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 09:31:08 +0100

    first commit

--- resources/js/components/nav.tsx
+++ resources/js/components/nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
     <>
       <nav className="bg-white shadow-md border-b py-2 px-2 ">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between"></div>
+            <div className="flex justify-between h-16"></div>
         </div>
       </nav>
     </>
