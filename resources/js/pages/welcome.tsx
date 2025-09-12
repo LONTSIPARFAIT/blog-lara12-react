e1d833f7d231c8e040a449b8ecf8270c163ae190
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 10:36:47 +0100

    first commit

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,6 +1,8 @@
+import PageProps from '@/types';
 import Nav from '@/components/nav';
+import { Post } from '@/types/post';
 
-export default function welcome() {
+export default function welcome(auth, posts, canRegister) : PageProps< {posts: Post[], canRegister: boolean }> {
   return (
     <div>
         <Nav />
