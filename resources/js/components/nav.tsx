8aa6daa3039d8a9791e0f8da7a3516af180c5595
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 09:29:08 +0100

    first commit

--- resources/js/components/nav.tsx
+++ resources/js/components/nav.tsx
@@ -1,10 +1,12 @@
 import { Link, usePage } from '@inertiajs/react'; 
 
 export default function Nav() {
-    const { auth } = usePage().props;
+    const { auth } = usePage().props as any;
   return (
-    <div>
-      Nav
-    </div>
+    <>
+      <nav className="bg-white shadow-md border-b py-2 px-2 ">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 flex justify-between"></div>
+      </nav>
+    </>
   )
 }
