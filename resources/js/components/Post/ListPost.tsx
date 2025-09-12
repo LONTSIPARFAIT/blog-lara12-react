6ef9e637317b77acf60c9f8d5a4f3785532cb65e
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-12 15:54:52 +0100

    first commit

--- resources/js/components/Post/ListPost.tsx
+++ resources/js/components/Post/ListPost.tsx
@@ -1,4 +1,5 @@
-import { usePage } from '@inertiajs/react';
+import { Props } from '@/types/post';
+import { usePage, router, Link } from '@inertiajs/react';
 import { useState } from 'react';
 
 export default function ListPost({posts, showAuthor = true} : Props) {
@@ -6,11 +7,28 @@ export default function ListPost({posts, showAuthor = true} : Props) {
     const { auth } = usePage().props as any;
     const [deletingId, setDeletingId] = useState<number | null>(null);
 
-    const handleDelete()
+    const handleDelete = (postId: number) => {
+        if (confirm("Etes-vous sûr de vouloir supprimer cet article ?")) {
+
+            setDeletingId(postId);
+            
+            router.delete(route('posts.destroy', postId), {
+                onSuccess: () => {
+                    setDeletingId(null);
+                },
+                onError: () => {
+                    setDeletingId(null);
+                    alert("Une erreur est survenue lors de la suppression de l'article.");
+                }
+            });
+        }
+    }
+
+    c
 
   return (
     <div>
-      
+      LisPost
     </div>
   )
 }
