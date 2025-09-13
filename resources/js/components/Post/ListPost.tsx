0294b5ff21d569183777d5278cb65a4a82bdb89b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-13 12:10:48 +0100

    first commit

--- resources/js/components/Post/ListPost.tsx
+++ resources/js/components/Post/ListPost.tsx
@@ -62,12 +62,12 @@ export default function ListPost({posts, showAuthor = true} : Props) {
                             <span className="">Par {post.author.name} </span>
                         )
                     }
-                    <span className="">{new Date(post.created_at).toLocaleDateString('fr_FR')} </span>
+                    <span classNam="">{new Date(post.created_at).toLocaleDateString('fr_FR')} </span>
                 </div>
             </CardContent>
 
-            <CardFooter>
-                
+            <CardFooter className='flex items-center justify-center'>
+                <Button />
             </CardFooter>
             
         </Card>
