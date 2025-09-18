8413c554e568b7665588ab18a96398f61bc84a83
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-09-18 07:18:44 +0100

    first commit

--- resources/js/components/Post/ListPost.tsx
+++ resources/js/components/Post/ListPost.tsx
@@ -69,7 +69,7 @@ export default function ListPost({posts, showAuthor = true} : Props) {
             </CardContent>
 
             <CardFooter className='flex items-center justify-center'>
-                <Button />
+                {/* <Button /> */}
                 <Button variant='ghost' onclick={()=>handleLike(post.id)} className={`transition-colors ${post.is_liked ? "text-red-600 hover:text-red-700" : "text-gray-600 hover:text-red-700"}`} >
                    <Heart className='h-6 w-6' fill={post.is_liked ? "currentColor" : "none"} /> 
                 </Button >
